b0aeffc8fd3c86bbd89ec88fe2652a675b218c24
Altan KRK <westcripp@users.noreply.github.com>
2019-01-14 18:31:27 +0300

    Update index.php

--- resources/themes/bootstrap/index.php
+++ resources/themes/bootstrap/index.php
@@ -33,6 +33,15 @@
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({});
         </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-77237517-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-77237517-2');
+</script>
     </head>
     <body>
 
