2b00a0205e1a0bb5ea2713d5cb4e0e85103d919f
Akhil Narang <akhilnarang.1999@gmail.com>
2019-01-17 19:46:19 +0530

    10 mins is enough
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- resources/DirectoryLister.php
+++ resources/DirectoryLister.php
@@ -632,7 +632,7 @@ class DirectoryLister {
 			}
 
 			$modificationTime = filemtime($realPath);
-			if (time() - $modificationTime > 900 || is_dir($relativePath)) {
+			if (time() - $modificationTime > 600 || is_dir($relativePath)) {
 
                             // Add the info to the main array
                             $directoryArray[pathinfo($relativePath, PATHINFO_BASENAME)] = array(
