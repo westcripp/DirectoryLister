bc1bd5474b5cc23cf2db06d7ab7064965b853926
Altan KRK <westcripp@users.noreply.github.com>
2019-01-20 17:13:55 +0300

    remove chitika ad

--- resources/themes/bootstrap/index.php
+++ resources/themes/bootstrap/index.php
@@ -30,17 +30,6 @@
             enable_page_level_ads: true
         });
         </script>
-<script type="text/javascript">
-  ( function() {
-    if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
-    var unit = {"calltype":"async[2]","publisher":"westcrip","width":728,"height":90,"sid":"Chitika Default"};
-    var placement_id = window.CHITIKA.units.length;
-    window.CHITIKA.units.push(unit);
-    document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
-}());
-</script>
-<script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
-
     </head>
     <body>
 
@@ -172,16 +161,6 @@
 
                             </tbody>
                         </table>
-<script type="text/javascript">
-  ( function() {
-    if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
-    var unit = {"calltype":"async[2]","publisher":"westcrip","width":728,"height":90,"sid":"Chitika Default"};
-    var placement_id = window.CHITIKA.units.length;
-    window.CHITIKA.units.push(unit);
-    document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
-}());
-</script>
-<script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
                     </div>
 
                 </div>
