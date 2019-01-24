ba1d0f66c6751ca09db3a6e960e01b587cf811e9
Akhil Narang <akhilnarang.1999@gmail.com>
2019-01-25 00:01:40 +0530

    test directly from server
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- download.php
+++ download.php
@@ -6,20 +6,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
 }
 $file=base64_decode($id);
 $file=str_replace("\n", '', $file);
-$url='https://downloads.sourceforge.net/project/resurrectionremix-pie/' . $file;
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'HEAD');
-curl_setopt($ch, CURLOPT_NOBODY, true);
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
-}
+//$url='https://downloads.sourceforge.net/project/resurrectionremix-pie/' . $file;
+//$ch = curl_init();
+//curl_setopt($ch, CURLOPT_URL, $url);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'HEAD');
+//curl_setopt($ch, CURLOPT_NOBODY, true);
+//$result = curl_exec($ch);
+//if (curl_errno($ch)) {
+//    echo 'Error:' . curl_error($ch);
+//}
 
-$url = curl_getinfo($ch, CURLINFO_REDIRECT_URL) . '?r=&ts='. time();
-print($url . "\n");
-curl_close ($ch);
+//$url = curl_getinfo($ch, CURLINFO_REDIRECT_URL) . '?r=&ts='. time();
+//print($url . "\n");
+//curl_close ($ch);
 header('Content-Description: '.basename($file));
 header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename="'.basename($file).'"');
@@ -27,5 +27,5 @@ header('Expires: 0');
 header('Cache-Control: must-revalidate');
 header('Content-Length: ' . filesize($file));
 header('Pragma: public');
-readfile($url);
+readfile($file);
 ?>
