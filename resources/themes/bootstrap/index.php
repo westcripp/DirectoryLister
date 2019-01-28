5c9db1260a6c1c76d2aa1b317dc33a0c286e016a
altan <korukaltan@gmail.com>
2019-01-28 22:39:02 +0300

    Revert "Test"
    This reverts commit 89ace014d550c0c47b418234677accb72543ae8c.

--- resources/themes/bootstrap/index.php
+++ resources/themes/bootstrap/index.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" href="<?php echo THEMEPATH; ?>/img/folder.png">
 
     <!-- STYLES -->
-    <link rel="stylesheet" href="<?php echo THEMEPATH; ?>/ccs/theme.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/css/style.css">
 
@@ -106,17 +106,7 @@
         </div>
         <?php endforeach; ?>
         <?php endif; ?>
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- Responsive -->
-          <ins class="adsbygoogle"
-          style="display:block"
-        data-ad-client="ca-pub-6660944894347757"
-        data-ad-slot="5459470774"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-     <script>
-       (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+
         <div id="directory-list-header">
             <div class="row">
                 <div class="col-md-7 col-sm-6 col-xs-10">File</div>
@@ -199,6 +189,17 @@
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Responsive -->
+<ins class="adsbygoogle"
+style="display:block"
+data-ad-client="ca-pub-6660944894347757"
+data-ad-slot="5459470774"
+data-ad-format="auto"
+data-full-width-responsive="true"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
     </center>
 
 </body>
