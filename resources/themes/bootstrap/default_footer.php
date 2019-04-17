99c447e55dee2415af668cb3806379447fdfbf23
altan <korukaltan@gmail.com>
2019-04-17 11:05:39 +0300

    Add redirection link

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,7 +1,7 @@
 <hr>
 
 <div class="footer">
-    ResurrectionRemix Downloads Page
+    Resurrection Remix OS Pie Downloads; For older versions visit <a href="https://archive.resurrectionremix.com/">Here</a>
     <br/>
     Powered by, <a href="http://www.directorylister.com">Directory Lister</a>
 </div>
